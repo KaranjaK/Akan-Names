2499bcb5193e14095c44ac9f9018d739245b0085
KK <karanjakelvin77@gmail.com>
2021-12-28 11:41:41 +0300

    It will comment out the form processing aspect of the commented form in the index.html file

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-function akanName(){
-    var day = new Date (document.getElementById('date').value);
-    var date = day.getDay();
-    var gender = document.getElementById("gender").value
-    if (gender == 'male'){
-        if(date == 0){
-            alert("You were born on Sunday so we name you Kwasi")
-        }
-        else if(date == 1){
-            alert("You were born on Monday so we name you Kwadwo")
-        }
-        else if(date == 2){
-            alert("You were born on Tuesday so we name you Kwabena")
-        }
-        else if(date == 3){
-            alert("You were born on Wednesday so we name you Kwaku")
-        }
-        else if(date == 4){
-            alert("You were born on Thursday so we name you Yaw")
-        }
-        else if(date == 5){
-            alert("You were born on Friday so we name you Kofi")
-        }
-        else
-            alert("You were born on Saturday so we name you Kwame")
-    }
-    else if (gender == 'female'){
-        if(date == 0){
-            alert("You were born on Sunday so we name you Akosua")
-        }
-        else if(date == 1){
-            alert("You were born on Monday so we name you Adwoa")
-        }
-        else if(date == 2){
-            alert("You were born on Tuesday so we name you Abenaa")
-        }
-        else if(date == 3){
-            alert("You were born on Wednesday so we name you Akua")
-        }
-        else if(date == 4){
-            alert("You were born on Thursday so we name you Yaa")
-        }
-        else if(date == 5){
-            alert("You were born on Friday so we name you Afua")
-        }
-        else
-            alert("You were born on Saturday so we name you Ama")
+// This function is attached to the commented form section of the HTML 
+
+// function akanName(){
+//     var day = new Date (document.getElementById('date').value);
+//     var date = day.getDay();
+//     var gender = document.getElementById("gender").value
+//     if (gender == 'male'){
+//         if(date == 0){
+//             alert("You were born on Sunday so we name you Kwasi")
+//         }
+//         else if(date == 1){
+//             alert("You were born on Monday so we name you Kwadwo")
+//         }
+//         else if(date == 2){
+//             alert("You were born on Tuesday so we name you Kwabena")
+//         }
+//         else if(date == 3){
+//             alert("You were born on Wednesday so we name you Kwaku")
+//         }
+//         else if(date == 4){
+//             alert("You were born on Thursday so we name you Yaw")
+//         }
+//         else if(date == 5){
+//             alert("You were born on Friday so we name you Kofi")
+//         }
+//         else
+//             alert("You were born on Saturday so we name you Kwame")
+//     }
+//     else if (gender == 'female'){
+//         if(date == 0){
+//             alert("You were born on Sunday so we name you Akosua")
+//         }
+//         else if(date == 1){
+//             alert("You were born on Monday so we name you Adwoa")
+//         }
+//         else if(date == 2){
+//             alert("You were born on Tuesday so we name you Abenaa")
+//         }
+//         else if(date == 3){
+//             alert("You were born on Wednesday so we name you Akua")
+//         }
+//         else if(date == 4){
+//             alert("You were born on Thursday so we name you Yaa")
+//         }
+//         else if(date == 5){
+//             alert("You were born on Friday so we name you Afua")
+//         }
+//         else
+//             alert("You were born on Saturday so we name you Ama")
           
-    }
-    else
-        alert("Sorry. We do not have an Akan Name for the Gender you Picked")
-}
+//     }
+//     else
+//         alert("Sorry. We do not have an Akan Name for the Gender you Picked")
+// }
